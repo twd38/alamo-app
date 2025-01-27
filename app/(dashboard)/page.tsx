30801ce19362c556b99bf14b3c85bc4b8e11f595
Will Davis <willdavis@Mac.localdomain>
2025-01-27 16:25:52 -0600

    remove product stuff

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { File, PlusCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import { getProducts } from '@/lib/db';
+// import { getProducts } from '@/lib/db';
 export default async function ProductsPage(
   props: {
     searchParams: Promise<{ q: string; offset: string }>;
@@ -10,10 +10,10 @@ export default async function ProductsPage(
   const searchParams = await props.searchParams;
   const search = searchParams.q ?? '';
   const offset = searchParams.offset ?? 0;
-  const { products, newOffset, totalProducts } = await getProducts(
-    search,
-    Number(offset)
-  );
+  // const { products, newOffset, totalProducts } = await getProducts(
+  //   search,
+  //   Number(offset)
+  // );
 
   return (
     <div className="">
--- lib/db.ts
+++ lib/db.ts
@@ -1,56 +1,56 @@
 import 'server-only';
-import { PrismaClient, Product } from '@prisma/client';
+// import { PrismaClient, Product } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export async function getProducts(
-  search: string,
-  offset: number
-): Promise<{
-  products: Product[];
-  newOffset: number | null;
-  totalProducts: number;
-}> {
-  if (search) {
-    const products = await prisma.product.findMany({
-      where: {
-        name: {
-          contains: search,
-          mode: 'insensitive'
-        }
-      },
-      take: 1000
-    });
-    return {
-      products,
-      newOffset: null,
-      totalProducts: 0
-    };
-  }
+// export async function getProducts(
+//   search: string,
+//   offset: number
+// ): Promise<{
+//   products: Product[];
+//   newOffset: number | null;
+//   totalProducts: number;
+// }> {
+//   if (search) {
+//     const products = await prisma.product.findMany({
+//       where: {
+//         name: {
+//           contains: search,
+//           mode: 'insensitive'
+//         }
+//       },
+//       take: 1000
+//     });
+//     return {
+//       products,
+//       newOffset: null,
+//       totalProducts: 0
+//     };
+//   }
 
-  if (offset === null) {
-    return { products: [], newOffset: null, totalProducts: 0 };
-  }
+//   if (offset === null) {
+//     return { products: [], newOffset: null, totalProducts: 0 };
+//   }
 
-  const [totalProducts, products] = await Promise.all([
-    prisma.product.count(),
-    prisma.product.findMany({
-      take: 5,
-      skip: offset
-    })
-  ]);
+//   const [totalProducts, products] = await Promise.all([
+//     prisma.product.count(),
+//     prisma.product.findMany({
+//       take: 5,
+//       skip: offset
+//     })
+//   ]);
 
-  const newOffset = products.length >= 5 ? offset + 5 : null;
+//   const newOffset = products.length >= 5 ? offset + 5 : null;
 
-  return {
-    products,
-    newOffset,
-    totalProducts
-  };
-}
+//   return {
+//     products,
+//     newOffset,
+//     totalProducts
+//   };
+// }
 
-export async function deleteProductById(id: number) {
-  await prisma.product.delete({
-    where: { id }
-  });
-}
+// export async function deleteProductById(id: number) {
+//   await prisma.product.delete({
+//     where: { id }
+//   });
+// }
