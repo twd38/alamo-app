5e47bdde1a3e149c1129d9e4ac4dc85d8cb4f49b
Will Davis <willdavis@Mac.localdomain>
2025-04-12 14:56:36 -0500

    fix deployment bug

--- src/app/content/[key]/route.ts
+++ src/app/content/[key]/route.ts
@@ -2,10 +2,13 @@ import { getPresignedDownloadUrl } from "@/lib/r2";
 import { NextRequest, NextResponse } from "next/server";
 
 // This route is used to get the presigned URL for the image
-export async function GET(request: NextRequest, { params }: { params: { key: string } }) {
-    const { key } = await params;
-    console.log("KEY: ", key);
-    const fullKey = "/content/" + key;
-    const url = await getPresignedDownloadUrl(fullKey);
-    return NextResponse.redirect(url);
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { key: string } }
+) {
+  const { key } = params;
+  console.log("KEY: ", key);
+  const fullKey = "/content/" + key;
+  const url = await getPresignedDownloadUrl(fullKey);
+  return NextResponse.redirect(url);
 }
