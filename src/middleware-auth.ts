ca4d796dcd70f33f11ffa9196c8d3a8bd0902c50
Will Davis <willdavis@Mac.localdomain>
2025-04-12 15:47:43 -0500

    revert changes for content route

--- src/middleware-auth.ts
+++
@@ -1,9 +0,0 @@
-// Export the auth middleware for authentication
-export { auth as middleware } from '@/lib/auth';
-
-// Don't invoke Middleware on some paths
-export const config = {
-  matcher: [
-    '/((?!login|register|api/auth|_next/static|_next/image|favicon.ico|content).*)'
-  ]
-}; 
