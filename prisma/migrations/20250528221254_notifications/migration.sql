e52adf0fa4d1a0172694508e85890686c474507a
Will Davis <willdavis@Mac.localdomain>
2025-05-28 17:14:07 -0500

    notifications model

---
+++ prisma/migrations/20250528221254_notifications/migration.sql
@@ -0,0 +1,23 @@
+-- CreateEnum
+CREATE TYPE "NotificationType" AS ENUM ('MENTION', 'ADDED');
+
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" TEXT NOT NULL,
+    "recipientId" TEXT NOT NULL,
+    "actorId" TEXT,
+    "type" "NotificationType" NOT NULL,
+    "link" TEXT,
+    "message" TEXT NOT NULL,
+    "readAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_recipientId_fkey" FOREIGN KEY ("recipientId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_actorId_fkey" FOREIGN KEY ("actorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
